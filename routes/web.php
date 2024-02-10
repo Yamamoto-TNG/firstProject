4638889374fde42fd144c9ce9d1afbec4c67f21c
Yamamoto-TNG <yamamoto.newgate@gmail.com>
2024-02-10 09:38:36 +0900

    ログイン　ルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// "Route"というツールを使うために必要な部品を取り込んでいます。
+use App\Http\Controllers\ProductController;
+// ProductControllerに繋げるために取り込んでいます
+use Illuminate\Support\Facades\Auth;
+// "Auth"という部品を使うために取り込んでいます。この部品はユーザー認証（ログイン）に関する処理を行います
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +19,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // ウェブサイトのホームページ（'/'のURL）にアクセスした場合のルートです
+    if (Auth::check()) {
+        // ログイン状態ならば
+        return redirect()->route('products.index');
+        // 商品一覧ページ（ProductControllerのindexメソッドが処理）へリダイレクトします
+    } else {
+        // ログイン状態でなければ
+        return redirect()->route('login');
+    }
 });
 
 Auth::routes();
+// Auth::routes();はLaravelが提供している便利な機能で
+// 一般的な認証に関するルーティングを自動的に定義してくれます
+// この一行を書くだけで、ログインやログアウト
+// パスワードのリセット、新規ユーザー登録などのための
+// ルートが作成されます。
+//　つまりログイン画面に用意されたビューのリンク先がこの1行で済みます
 
 // 商品情報一覧画面
 Route::get('/home', [App\Http\Controllers\ProductController::class, 'showList'])->name('home');
